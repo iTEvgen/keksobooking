934ccafc6184c7729484dbb0729a058fa4b41c7a
Evgen <e.arzhilovskiy@outlook.com>
2023-10-09 21:43:27 +0500

    Второе домашнее задание домашнее задание не получается выполнить так как EsLint выдает ошибку, было принято установить посланию версию

---
+++ js/main.js
@@ -0,0 +1,23 @@
+const getRandomInteger = (min, max) => {
+  if (min < 0 || max < 0) {
+    return -1;
+  }
+
+  if (min > max) {
+    [min, max] = [max, min];
+  }
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+const getCalculateCoordinates = (min, max, decimalPlace) => {
+  if (min < 0 || max < 0) {
+    return -1;
+  }
+
+  if (min > max) {
+    [min, max] = [max, min];
+  }
+  const randomFloat = Math.random() * (max - min + 1) + min;
+  return randomFloat.toFixed(decimalPlace);
+};
+
