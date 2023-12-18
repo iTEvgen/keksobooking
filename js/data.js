62e4db798507d36865c0677510e02aa6509a13ca
Evgen <e.arzhilovskiy@outlook.com>
2023-12-18 17:16:15 +0500

    Задание четвертой домашней работы(Рефакторинг) Код был разнесен на мой взгляд логичнее, также были добавлены новый файлы для координат и на проверку повторяющего элемента.

--- js/data.js
+++ js/data.js
@@ -1,3 +1,9 @@
+import { getRandomInteger, getRandomArrayElement } from './util.js';
+import { getCheckDuplicatesArray } from './duplicate-сhecker.js';
+import { housingLocationObject } from './map-сoordinates.js';
+
+const MAX_STEPS = 10;
+
 const NUMBER_ARRAY = [
   '01',
   '02',
@@ -94,16 +100,33 @@ const Coordinates = {
   MAX_Y: 139.80,
 };
 
-export {
-  NUMBER_ARRAY,
-  AD_TITLES,
-  AD_DESCRIPTION,
-  TYPE_HOUSING,
-  FEATURE_VALUES,
-  DATE_TIME,
-  IMAGES,
-  ROOMS,
-  GUESTS,
-  PRICE,
-  Coordinates,
+const title = getRandomArrayElement(AD_TITLES);
+
+const housingInformationObject = () => {
+  const location = housingLocationObject();
+  const address = `x: ${location.x}, y: ${location.y}`;
+
+  return {
+    author: {
+      avatar: `img/avatars/user${getRandomArrayElement(NUMBER_ARRAY)}.svg`,
+    },
+    offer: {
+      title: title,
+      address: address,
+      price: getRandomArrayElement(PRICE),
+      type: getRandomArrayElement(TYPE_HOUSING),
+      rooms: getRandomArrayElement(ROOMS),
+      guests: getRandomArrayElement(GUESTS),
+      checkin: getRandomArrayElement(DATE_TIME),
+      checkout: getRandomArrayElement(DATE_TIME),
+      features: getCheckDuplicatesArray(FEATURE_VALUES),
+      description: getRandomArrayElement(AD_DESCRIPTION),
+      photos: Array.from({length: getRandomInteger(1, IMAGES.length - 1)}, () => getRandomArrayElement(IMAGES)),
+    },
+    location: location,
+  };
 };
+
+const housingInformationArray = new Array(MAX_STEPS).fill(null).map(() => housingInformationObject());
+
+export { housingInformationArray, FEATURE_VALUES, Coordinates };
--- js/util.js
+++ js/util.js
@@ -1,10 +1,3 @@
-import {
-  FEATURE_VALUES,
-  Coordinates,
-} from './data.js';
-
-const MANTISSA = 5;
-
 const getRandomInteger = (min, max) => {
   if (min < 0 || max < 0) {
     return -1;
@@ -30,18 +23,6 @@ const getCalculateCoordinates = (min, max, decimalPlace) => {
 
 const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
 
-const getCheckDuplicatesArray = (array) => {
-  const featureLength = getRandomInteger(1, array.length - 1);
-  const features = Array.from({ length: featureLength }, () => getRandomArrayElement(FEATURE_VALUES));
-  const uniqueFeatures = Array.from(new Set(features));
-  return uniqueFeatures;
-};
-
-const housingLocationObject = () => ({
-  x: getCalculateCoordinates(Coordinates.MIN_X, Coordinates.MAX_X, MANTISSA),
-  y: getCalculateCoordinates(Coordinates.MIN_Y, Coordinates.MAX_Y, MANTISSA),
-});
-
 // const getStringCount = (text) => {
 //   const length = text.length;
 //   return (length >= 30 && length <= 100) ? console.log('Все отлично заголовок нужной длины.') : console.log('Все пропало залогово не подходит.');
@@ -49,7 +30,6 @@ const housingLocationObject = () => ({
 
 export {
   getRandomInteger,
+  getCalculateCoordinates,
   getRandomArrayElement,
-  getCheckDuplicatesArray,
-  housingLocationObject,
 };
