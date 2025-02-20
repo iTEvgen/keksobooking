b9a58176e032e2b8ada22772d1e4480622b282e2
Evgen <e.arzhilovskiy@outlook.com>
2025-02-20 15:52:07 +0500

    test end

--- js/main.js
+++ js/main.js
@@ -1,4 +1,15 @@
 import { housingInformationArray } from './data.js';
+// import {objTestJs} from './test-js.js';
 
-housingInformationArray();
+// housingInformationArray();
+// objTestJs();
 // console.log(housingInformationArray);
+(
+  ()=> {
+    const obj1 = {};
+    const obj2 = {name: 'obj2'};
+
+    Reflect.setPrototypeOf(obj1, obj2);
+    console.log(obj1.name);
+  }
+)();
