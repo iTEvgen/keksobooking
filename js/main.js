e4e0412bb8e5e76e34bbb5ff732ab0ab066d6ab0
Evgen <e.arzhilovskiy@outlook.com>
2025-03-26 23:12:42 +0500

    file data.js 1.Добавлены новые импорты. 2.Добавлен код для взаимодействия с интерфейсом, валидация формы и генерации объекта со свойствами.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,9 @@
-import { housingInformationArray } from './data.js';
+import { housingInformationArray } from'./data.js';
+import {renderCardToFragment} from'./template-renderer.js';
+import { formHandler } from './form-handler.js';
 
-housingInformationArray();
-// console.log(housingInformationArray);
+const cardElement = document.querySelector('#card').content.querySelector('.popup');
+const firstCardData = housingInformationArray[0];
+
+formHandler();
+renderCardToFragment(cardElement, firstCardData, '#map-canvas');
