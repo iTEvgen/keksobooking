6b3bb47da10df9d202737aee4780752cd7e5b617
Evgen <e.arzhilovskiy@outlook.com>
2025-05-04 16:55:43 +0500

    file util.js 1.Весь код был переписан

--- js/util.js
+++ js/util.js
@@ -1,4 +1,4 @@
-const getRandomInteger = (min, max, decimalPlace = 0) => {
+const validateAndSwap = (min, max) => {
   if (min < 0 || max < 0) {
     return -1;
   }
@@ -7,19 +7,50 @@ const getRandomInteger = (min, max, decimalPlace = 0) => {
     [min, max] = [max, min];
   }
 
-  const random = Math.floor(Math.random() * (max - min + 1)) + min;
+  return { min, max };
+};
 
-  return decimalPlace ? random.toFixed(decimalPlace) : random;
+const getRandomInteger = (min, max) => {
+  const { min: validatedMin, max: validatedMax } = validateAndSwap(min, max);
+  if (validatedMin === -1) {
+    return -1;
+  }
+
+  const random = Math.floor(Math.random() * (validatedMax - validatedMin + 1)) + validatedMin;
+  return random;
+};
+
+const DECIMAL_BASE = 10;
+
+const getRandomFloatNumber = (min, max, decimalPlace) => {
+  const { min: validatedMin, max: validatedMax } = validateAndSwap(min, max);
+  if (validatedMin === -1) {
+    return -1;
+  }
+
+  const randomNumber = Math.random() * (validatedMax - validatedMin) + validatedMin;
+  const decimalShiftFactor = Math.pow(DECIMAL_BASE, decimalPlace);
+  return Math.round(randomNumber * decimalShiftFactor) / decimalShiftFactor;
 };
 
 const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
+
+// Генерация иконки для библиотеки leaflet
+const createIcon = (url, size, anchor) => L.icon({
+  iconUrl: url,
+  iconSize: size,
+  iconAnchor: anchor
+});
+
+export {
+  getRandomInteger,
+  getRandomArrayElement,
+  createIcon,
+  getRandomFloatNumber
+};
+
 
 // const getStringCount = (text) => {
 //   const length = text.length;
 //   return (length >= 30 && length <= 100) ? console.log('Все отлично заголовок нужной длины.') : console.log('Все пропало залогово не подходит.');
 // };
-
-export {
-  getRandomInteger,
-  getRandomArrayElement,
-};
